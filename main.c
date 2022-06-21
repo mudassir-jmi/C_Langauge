a11b120dbb5e07463d0846aa61e32a6c0252067a
Md Mudassir Akhter <mudassirakhter420@gmail.com>
2022-06-21 21:44:23 +0530

    arithimetic instruction in c

---
+++ main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+   float b, h, a;
+   printf("enter base & height: ");
+   scanf("%f %f",&b,&h);
+   a = (b * h ) / 2;
+   printf("Area of triangle is %f ", a);
+   return 0;
+
+}
