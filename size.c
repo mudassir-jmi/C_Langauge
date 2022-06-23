38a66007bb44b22fa5e067b367cd2030c58d7195
Md Mudassir Akhter <mudassirakhter420@gmail.com>
2022-06-23 19:22:45 +0530

    change in size file

--- size.c
+++ size.c
@@ -1,7 +1,19 @@
 #include<stdio.h>
 int main(int argc, char const *argv[])
 {
-    char x;
-    printf("%d",sizeof(x));
-    return 0;
+    // char x;
+    // printf("%d",sizeof(x));
+    // return 0;
+
+    // int x;
+    // printf("%d",sizeof(x));
+    // return 0;
+
+    // float x;
+    // printf("%d",sizeof(x));
+    // return 0;
+
+    // double x;
+    // printf("%d",sizeof(x));
+    // return 0;
 }
