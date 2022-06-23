46efcb96cec4f284469527f83dca20ea8cd66034
Md Mudassir Akhter <mudassirakhter420@gmail.com>
2022-06-23 22:06:37 +0530

    new commit

--- and.c
+++ and.c
@@ -1,23 +1,23 @@
 #include<stdio.h>
 int main()
 {
-    // int x;
-    // x = 30 | 25;
-    // printf("%d",x);  // AND operator
-    // return 0;
+    int x;
+    x = 30 & 25;
+    printf("%d",x);  // AND operator
+    return 0;
 
     // int x;
     // x = 30 | 25;
     // printf("%d",x);  // OR operator
     // return 0;
 
-    int x;
-    x = 50 << 2;     // shift left
-     printf("%d",x);
-    return 0;
+    // int x;
+    // x = 50 << 2;     // shift left
+    //  printf("%d",x);
+    // return 0;
 
-    int y;
-    y = 50 >> 2;     // shift right
-     printf("%d",y);
-    return 0;
+    // int y;
+    // y = 50 >> 2;     // shift right
+    //  printf("%d",y);
+    // return 0;
 }
